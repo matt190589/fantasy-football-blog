3809f4fa282467ad7ed5b7c29402b2be19e35ed9
matt190589 <matthew.doherty97@gmail.com>
2023-02-14 13:07:55 +0000

    Basic Search component added but not ready for addition to live site. Need to implement blog tags to align with filter function

---
+++ components/Search.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Search() {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const filterPosts = (event) => {
+    console.log(event.target.value);
+    const value = event.target.value.toLowerCase();
+    const filteredPosts = allPosts.filter((user) =>
+      `${player.name.last} ${club.name}`.toLowerCase().includes(value)
+    );
+    setAllPosts(filteredPosts);
+  };
+
+  return (
+    <div className="search">
+      <div>
+        <input
+          className="search-box"
+          placeholder="Enter team or player"
+          onInput={filterPosts}
+        />
+      </div>
+    </div>
+  );
+}
+
+//Next steps on this function
+//Appropriate tags or arrays should be added to the markdown files
+//Import these put them into a state on the app page
+// Pass the object down and replace the dummy props in the above function
+//Send target value down as props
+//Adjust the mapping on the results and test filtering
+//Add CSS and positioning to the search functionality
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import CardPost from "../components/CardPost";
+import Search from "../components/Search";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -26,6 +27,7 @@ export default function Home({ allPostsData }) {
           discuss the best Ins and Outs before the upcoming transfer deadline
           while sharing my own wins and woes along the way.
         </p>
+        <Search />
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
